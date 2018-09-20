e6102e921012f5765e754a04d288032bb73d3996
caseydzuong <cdzuong@gmail.com>
2018-09-19 21:15:44 -0700

    MyArrayTester files all perfect, completely pass all tests. Image processing files for grayscale and noise perfect. Image processing files for blur and flipped still need revision.

--- src/arrays/MyArray.java
+++ src/arrays/MyArray.java
@@ -1,13 +1,18 @@
 package arrays;
 
-/** Class MyArray. Contains an array of integers and methods to manipulate it.
- *  Fill in code where needed.  */
+import java.util.Arrays;
+
+/**
+ * Class MyArray. Contains an array of integers and methods to manipulate it.
+ * Fill in code where needed.
+ */
 public class MyArray {
     private int[] array;
     private int currentIndex = 0; // current size of the array
 
     /**
      * Constructor that takes the maximum size
+     *
      * @param maxSize the largest number of elements the array can hold
      */
     public MyArray(int maxSize) {
@@ -15,27 +20,43 @@ public class MyArray {
     }
 
 
-    /** If the array has space, adds a given element to the array and returns true;
-     *  If the array has no more space, prints an error and returns false.
+    /**
+     * If the array has space, adds a given element to the array and returns true;
+     * If the array has no more space, prints an error and returns false.
+     *
      * @param elem integer element to add to the array
      * @return true if was able to add the element, and false otherwise
      */
     public boolean addElement(int elem) {
-       // FILL IN CODE
+        // FILL IN CODE
 
-        return true; // remember to change this
+        if (currentIndex < array.length) {
+            array[currentIndex] = elem;
+            currentIndex++;
+            return true;
+        }
+        return false;
     }
+
+
+    // remember to change this
+
 
     /**
      * Return the index of the element of the array that is equal to elem, or -1
      * if not found
+     *
      * @param elem integer element
      * @return index of the first occurrence of this element
      */
     public int find(int elem) {
         int index = -1;
         // FILL IN CODE
-
+        for (int i = array.length - 1; i >= 0; i--) {
+            if (array[i] == elem) {
+                index = i;
+            }
+        }
         return index;   // remember to change this
 
     }
@@ -43,22 +64,44 @@ public class MyArray {
     /**
      * Remove the first occurrence of elem in the array. You are not allowed to use
      * extra memory except for one loop variable i.
+     *
      * @param elem integer element
      * @return true if removal was successful, and false otherwise
      */
     public boolean remove(int elem) {
         // FILL IN CODE
 
+
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == elem) {
+                do {
+                    array[i] = array[i + 1];
+                    i++;
+                } while (i < array.length - 1);
+                return true;
+            }
+        }
+
+        return false;
         // Remove the element at i = index
 
         // Shift elements after elem to the left
 
-        return true; // remember to change this
+
     }
 
-    /** Reverse a given array. You may NOT use  extra memory apart from two int variables.
-      */
+    /**
+     * Reverse a given array. You may NOT use  extra memory apart from two int variables.
+     */
     public void reverse() {
+        int temp;
+
+        for (int i = 0; i < currentIndex / 2; i++) {
+            temp = array[i];
+            array[i] = array[currentIndex - i - 1];
+            array[currentIndex - i - 1] = temp;
+        }
+
 
         // FILL IN CODE
     }
@@ -72,7 +115,16 @@ public class MyArray {
      */
     public String toString() {
         String res = "";
-       // FILL IN CODE
+        // FILL IN CODE
+        for (int i = 0; i < currentIndex; i++) {
+            if (i != currentIndex - 1) {
+                res += array[i] + " ";
+            } else {
+                res += array[i];
+            }
+
+
+        }
 
         return res;
     }
@@ -83,32 +135,97 @@ public class MyArray {
      * inside the method.
      */
     public void removeEveryOtherElement() {
-       // FILL IN CODE
+        // FILL IN CODE
+
+            int[] evens = new int[(array.length + 1) / 2];
+
+
+        int i = 0;
+        while (i < evens.length) {
+            for (int j = 0; j < array.length; j += 2) {
+                evens[i] = array[j];
+                i++;
+            }
+        }
+
+
+        this.array = evens;
+        currentIndex = array.length - 1;
 
     }
 
-    /** Repeat the array n times.
-     *  You are allowed to use an extra int[] array of size currentIndex * n.
-     *  Once you fill this extra array of values, you can point this.array to
-     *  this new array.
+    /**
+     * Repeat the array n times.
+     * You are allowed to use an extra int[] array of size currentIndex * n.
+     * Once you fill this extra array of values, you can point this.array to
+     * this new array.
+     *
      * @param n how many times to repeat elements
      */
     public void repeat(int n) {
-       // FILL IN CODE
+        // FILL IN CODE
+        int[] multiply = new int[n * currentIndex];
 
+        int j = 0;
+        while (j < multiply.length) {
+            for (int i = 0; i < currentIndex; i++) {
+                multiply[j] = array[i];
+                j++;
+            }
+
+        }
+
+        this.array = multiply;
+        currentIndex = array.length;
+
+
+        /*
+        int old = array.length;
+        int newLength = multiply.length;
+
+        Arrays.copyOf(old, newLength);
+
+        for (int i = 0; i < multiply.length; i++) {
+            array[i] = multiply[i];
+
+        }
+
+        */
     }
 
     /**
      * Return the largest common element between array and otherArray.array.
      * You are not allowed to use an extra array etc (NO extra memory except for
      * loop variable and largestCommonMax).
+     *
      * @param otherArray another MyArray reference
      * @return largest common element between this and otherArray
      */
     public int findLargestCommonElement(MyArray otherArray) {
         // FILL IN CODE
 
-       return -1;
-    }
+        int largestCommonMax = 0;
+        if (array.length >= otherArray.array.length) {
+            for (int i = 0; i < array.length; i++) {
+                for (int j = 0; j < otherArray.array.length; j++) {
+                    if (array[i] == otherArray.array[j] && array[i] >= largestCommonMax) {
+                        largestCommonMax = array[i];
+                    }
+                }
 
+            }
+        } else {
+            for (int i = 0; i < otherArray.array.length; i++) {
+                for (int j = 0; j < array.length; j++) {
+                    if (array[i] == otherArray.array[j] && array[i] >= largestCommonMax) {
+                        largestCommonMax = array[i];
+                    }
+                }
+
+            }
+        }
+
+
+        return largestCommonMax;
+    }
 }
--- src/imageManipulation/ImageProcessor.java
+++ src/imageManipulation/ImageProcessor.java
@@ -1,10 +1,16 @@
+
+
 package imageManipulation;
+
+import java.awt.image.BufferedImage;
+import java.util.Random;
 
 public class ImageProcessor {
     private Picture myImage; // reference to the Picture object
 
     /**
      * Constructor: creates a Picture from the given image file
+     *
      * @param imageFilename the file with the image
      */
     public ImageProcessor(String imageFilename) {
@@ -12,7 +18,8 @@ public class ImageProcessor {
         this.myImage = new Picture(imageFilename);
     }
 
-    /** Reset myImage by loading the image from the given file
+    /**
+     * Reset myImage by loading the image from the given file
      *
      * @param imageFilename name of the file with an image
      */
@@ -21,16 +28,29 @@ public class ImageProcessor {
         this.myImage = new Picture(imageFilename);
     }
 
-    /** Convert myImage to grayscale using the following formula:
-     *  New r, g, b values of a pixel should be set to the following sum:
-     *  30% of current red value of the pixel  +
-     *  59% of current green value of the pixel +
-     *  11% of blue value of the pixel.
-     *
+    /**
+     * Convert myImage to grayscale using the following formula:
+     * New r, g, b values of a pixel should be set to the following sum:
+     * 30% of current red value of the pixel  +
+     * 59% of current green value of the pixel +
+     * 11% of blue value of the pixel.
      */
     public void convertToGrayscale() {
         // FILL IN CODE
 
+
+        for (int i = 0; i < myImage.getWidth(); i++) {
+            for (int j = 0; j < myImage.getHeight(); j++) {
+                int red = myImage.getPixel(i, j).getRed();
+                int green = myImage.getPixel(i, j).getGreen();
+                int blue = myImage.getPixel(i, j).getBlue();
+                double avg = .3 * red + .59 * green + .11 * blue;
+                int avgCast = (int) avg;
+
+                Pixel pixel = new Pixel(avgCast, avgCast, avgCast);
+                myImage.setPixel(i, j, pixel);
+            }
+        }
         // Use myImage.getWidth() to find the width of the image
         // and myImage.getHeight to get the height
         // To access the pixel at (i,j), use myImage.getPixel(i, j).
@@ -38,31 +58,95 @@ public class ImageProcessor {
         // Use setPixel method to set new values for a particular pixel.
     }
 
-    /** Flips image horizontally */
+    /**
+     * Flips image horizontally
+     */
     public void flipHorizontally() {
         // FILL IN CODE
 
+        Pixel dot;
+        int diff;
+        int newDot;
+        Pixel temp;
+        for (int i = 0; i < myImage.getHeight(); i++) {
+            for (int j = 0; j < myImage.getWidth(); j++) {
+                //     myImage.getPixel(i, j); set as variable
+                // swap pixels with setPixel method
+                diff = (myImage.getWidth() / 2) - j;
+                newDot = (myImage.getWidth() / 2) + diff;
+                dot = myImage.getPixel(i, j);
+                temp = myImage.getPixel(i, newDot);
+                myImage.setPixel(i, newDot, dot);
+                myImage.setPixel(i, j, temp);
+
+            }
+        }
+
+
     }
 
 
-    /** Add random noise to myImage. Go over each pixel, and add
-     *  a random number from 0 to 255 to each of the components. Make
-     *  sure you adjust the range so that the max for each component is 255.
+    /**
+     * Add random noise to myImage. Go over each pixel, and add
+     * a random number from 0 to 255 to each of the components. Make
+     * sure you adjust the range so that the max for each component is 255.
      */
     public void addNoise() {
         // FILL IN CODE
+        // make sure not > 255
+        // create new pixels EACH for r g b
+        Random random = new Random();
+
+        for (int i = 0; i < myImage.getWidth(); i++) {
+            for (int j = 0; j < myImage.getHeight(); j++) {
+                int red = myImage.getPixel(i, j).getRed() + random.nextInt(256);
+                int green = myImage.getPixel(i, j).getGreen() + random.nextInt(256);
+                int blue = myImage.getPixel(i, j).getBlue() + random.nextInt(256);
+
+                if (red > 255) {
+                    do {
+                        red = myImage.getPixel(i, j).getRed() + random.nextInt(256);
+                    } while (red > 255);
+                }
+                if (green > 255) {
+                    do {
+                        green = myImage.getPixel(i, j).getGreen() + random.nextInt(256);
+                    } while (green > 255);
+                }
+                if (blue > 255) {
+                    do {
+                        blue = myImage.getPixel(i, j).getBlue() + random.nextInt(256);
+                    } while (blue > 255);
+                }
+
+                Pixel pixel = new Pixel(red, green, blue);
+                myImage.setPixel(i, j, pixel);
+            }
+        }
     }
 
 
+    /**
+     * Make the image blurry using convolution
+     */
 
-    /** Make the image blurry using convolution */
+
     public void blur() {
         // FILL IN CODE
+// write a helper function (a new method), returns array of pixels of itself plus its neighbors
+        // i, j, (avg r g b of that array grid)
+
+        // ignore edges because out of bound
+
+
+
+
 
     }
 
     /**
      * Store this picture in the file with the specified name
+     *
      * @param imageFilename name of the file where th
      */
     public void store(String imageFilename) {
@@ -70,3 +154,5 @@ public class ImageProcessor {
     }
 
 }
+
+
